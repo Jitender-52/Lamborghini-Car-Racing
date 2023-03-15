653bae3952c3df8fc69d6173ac0849ba9dab9bca
Jitender <jitendersharmateharki@gmail.com>
2023-03-15 11:06:52 +0530

    Fourth Commit

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-var audio = new Audio('sound.mp3');
+// var audio = new Audio('sound.mp3');
+// audio.play();
+
+var audio = document.createElement('Audio');
+audio.setAttribute('src', 'sound.mp3');
+console.log("hekafd");
+audio.loop = true;
 audio.play();
